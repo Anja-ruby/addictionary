9c0562dd25038a97da74a8c2ba22bbc31fe9355a
Anja <anja.lachowicz@gmail.com>
2013-07-10 21:19:27 +0200

    fixed renaming

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Addictionary::Application.config.session_store :cookie_store, key: '_slapa_session'
+Addictionary::Application.config.session_store :cookie_store, key: '_Addictionary_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
