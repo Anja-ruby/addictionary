687fa7e75d27f5cd2c8fea29e994d479cd4d9d18
Anja <anja.lachowicz@gmail.com>
2013-07-10 22:57:21 +0200

    words

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +7,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Word.create(:polskie => "kot", :angielskie => "cat")
+Word.create(:polskie => "dom", :angielskie => "house")
+Word.create(:polskie => "mama", :angielskie => "mother")
+Word.create(:polskie => "dzień", :angielskie => "day")
+Word.create(:polskie => "tydzień", :angielskie => "week")
+Word.create(:polskie => "kwiecień", :angielskie => "april")
+Word.create(:polskie => "czerwiec", :angielskie => "june")
+Word.create(:polskie => "maj", :angielskie => "may")
+Word.create(:polskie => "lipiec", :angielskie => "july")
+Word.create(:polskie => "cecha", :angielskie => "property, quality, characteristic ")
+Word.create(:polskie => "ciasto", :angielskie => "cake, pie")
+Word.create(:polskie => "fala", :angielskie => "wave, torrent")
+Word.create(:polskie => "bajka", :angielskie => "fable, fairy tale")
+Word.create(:polskie => "plik", :angielskie => "file")
